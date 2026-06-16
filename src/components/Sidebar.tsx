7eabc9e7213c1ae4bddeee1ae2ba46f5982cc066
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-16 19:18:37 -0400

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,1 +1,33 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
 
+      <h1>VisionPro ERP</h1>
+      <p>Optical Management</p>
+
+      <div className="grupo">
+        <h4>GENERAL</h4>
+
+        <ul>
+          <li>Dashboard</li>
+          <li>Pacientes</li>
+          <li>Agenda</li>
+          <li>Historia Clínica</li>
+        </ul>
+      </div>
+
+      <div className="grupo">
+        <h4>INVENTARIO Y VENTAS</h4>
+
+        <ul>
+          <li>Recetas</li>
+          <li>Ordenes</li>
+          <li>Ventas</li>
+        </ul>
+      </div>
+
+    </aside>
+  );
+}
+
+export default Sidebar;
