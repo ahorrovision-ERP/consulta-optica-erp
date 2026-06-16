d6dd9f0c3eb4061ee5e4068ed155ac1a1f52b643
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-16 19:18:16 -0400

    Update StatCard.tsx

--- src/components/StatCard.tsx
+++ src/components/StatCard.tsx
@@ -1,1 +1,15 @@
+type Props = {
+  titulo: string;
+  valor: string;
+};
 
+function StatCard({ titulo, valor }: Props) {
+  return (
+    <div className="card">
+      <h3>{titulo}</h3>
+      <h2>{valor}</h2>
+    </div>
+  );
+}
+
+export default StatCard;
