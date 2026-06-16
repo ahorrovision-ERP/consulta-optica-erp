da072f4c9f9e873357d1e808bcd9480b2d677ec8
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-16 19:20:58 -0400

    Update HistoriaClinica.tsx

--- src/pages/HistoriaClinica.tsx
+++ src/pages/HistoriaClinica.tsx
@@ -1,1 +1,13 @@
+import MainLayout from "../layout/MainLayout";
 
+function HistoriaClinica() {
+  return (
+    <MainLayout>
+
+      <h1>Historia Clínica</h1>
+
+    </MainLayout>
+  );
+}
+
+export default HistoriaClinica;
