0bb75d6977232ac44328ca7e99166bddc772f791
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-16 19:22:44 -0400

    Update Ordenes.tsx

--- src/pages/Ordenes.tsx
+++ src/pages/Ordenes.tsx
@@ -1,1 +1,13 @@
+import MainLayout from "../layout/MainLayout";
 
+function Recetas() {
+  return (
+    <MainLayout>
+
+      <h1>Recetas</h1>
+
+    </MainLayout>
+  );
+}
+
+export default Recetas;
