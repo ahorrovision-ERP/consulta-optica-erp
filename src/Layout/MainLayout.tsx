76b4d23139421cb8da644ed31ea194c729cabecc
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-16 19:30:52 -0400

    Update MainLayout.tsx

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,1 +1,21 @@
+import Sidebar from "../components/Sidebar";
 
+type Props = {
+  children: React.ReactNode;
+};
+
+function MainLayout({ children }: Props) {
+  return (
+    <div className="container">
+
+      <Sidebar />
+
+      <main className="contenido">
+        {children}
+      </main>
+
+    </div>
+  );
+}
+
+export default MainLayout;
