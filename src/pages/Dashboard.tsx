edb88384847eff7361ab3d5ba789fbf3eebe5be7
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-18 13:34:11 -0400

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,11 @@
 import MainLayout from "../layout/MainLayout";
 import StatCard from "../components/StatCard";
+import TopBar from "../components/TopBar";
 
 function Dashboard() {
   return (
     <MainLayout>
-
+<TopBar />
       <h1>Dashboard Overview</h1>
 
       <div className="cards">
