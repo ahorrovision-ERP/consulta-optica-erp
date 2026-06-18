17df9cfcfeebc0cc7b78f66b7c16a684cabdbc47
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-18 15:07:42 -0400

    Update DataTable.tsx

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,1 +1,54 @@
+interface DataTableProps {
+  columns: string[];
+  data: string[][];
+}
 
+function DataTable({ columns, data }: DataTableProps) {
+  return (
+    <div className="card">
+      <table
+        style={{
+          width: "100%",
+          borderCollapse: "collapse"
+        }}
+      >
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th
+                key={index}
+                style={{
+                  textAlign: "left",
+                  padding: "15px",
+                  borderBottom: "1px solid #ddd"
+                }}
+              >
+                {column}
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <td
+                  key={cellIndex}
+                  style={{
+                    padding: "15px",
+                    borderBottom: "1px solid #eee"
+                  }}
+                >
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default DataTable;
