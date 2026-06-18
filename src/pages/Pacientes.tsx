3a6bca4f5bd80fd3a9f32a902950e55202697de8
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-18 18:28:44 -0400

    Update Pacientes.tsx

--- src/pages/Pacientes.tsx
+++ src/pages/Pacientes.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 import MainLayout from "../layout/MainLayout";
 import PageHeader from "../components/PageHeader";
 import SearchBar from "../components/SearchBar";
 import DataTable from "../components/DataTable";
 
+import PatientModal from "../components/PatientModal";
+import PatientForm from "../components/PatientForm";
+
 function Pacientes() {
+
+  const [openModal, setOpenModal] = useState(false);
 
   const columnas = [
     "ID",
@@ -15,11 +22,11 @@ function Pacientes() {
   ];
 
   const pacientes = [
-    ["1","Juan Pérez","+56 9 4567 8912","45","Santiago","✏️ 🗑️"],
-    ["2","María González","+56 9 8123 4567","29","Macul","✏️ 🗑️"],
-    ["3","Pedro Ramírez","+56 9 9988 7766","61","Ñuñoa","✏️ 🗑️"],
-    ["4","Elena García","+56 9 4455 6677","37","Providencia","✏️ 🗑️"],
-    ["5","Carlos Muñoz","+56 9 2233 8899","52","La Florida","✏️ 🗑️"]
+    ["1", "Juan Pérez", "+56 9 4567 8912", "45", "Santiago", "✏️ 🗑️"],
+    ["2", "María González", "+56 9 8123 4567", "29", "Macul", "✏️ 🗑️"],
+    ["3", "Pedro Ramírez", "+56 9 9988 7766", "61", "Ñuñoa", "✏️ 🗑️"],
+    ["4", "Elena García", "+56 9 4455 6677", "37", "Providencia", "✏️ 🗑️"],
+    ["5", "Carlos Muñoz", "+56 9 2233 8899", "52", "La Florida", "✏️ 🗑️"]
   ];
 
   return (
@@ -44,7 +51,10 @@ function Pacientes() {
           <SearchBar />
         </div>
 
-        <button className="btn-primary">
+        <button
+          className="btn-primary"
+          onClick={() => setOpenModal(true)}
+        >
           + Nuevo
         </button>
 
@@ -62,6 +72,15 @@ function Pacientes() {
         columns={columnas}
         data={pacientes}
       />
+
+      {/* Modal */}
+      <PatientModal
+        isOpen={openModal}
+        title="Nuevo Paciente"
+        onClose={() => setOpenModal(false)}
+      >
+        <PatientForm />
+      </PatientModal>
 
     </MainLayout>
   );
