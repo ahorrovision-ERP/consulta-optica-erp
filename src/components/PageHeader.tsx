25ae29265cf71954a816d6f5ce427cdb7d52fe79
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-18 15:08:42 -0400

    Update PageHeader.tsx

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,1 +1,26 @@
+interface PageHeaderProps {
+  titulo: string;
+  subtitulo?: string;
+}
 
+function PageHeader({ titulo, subtitulo }: PageHeaderProps) {
+  return (
+    <div
+      style={{
+        marginBottom: "30px"
+      }}
+    >
+      <h1>{titulo}</h1>
+
+      <p
+        style={{
+          color: "gray"
+        }}
+      >
+        {subtitulo}
+      </p>
+    </div>
+  );
+}
+
+export default PageHeader;
