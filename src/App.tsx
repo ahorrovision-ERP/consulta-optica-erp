54d8c12940952d6747f87d5b2b170007f5380d82
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-06-18 15:20:04 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
+import Pacientes from "./pages/Pacientes";
+import Agenda from "./pages/Agenda";
+import HistoriaClinica from "./pages/HistoriaClinica";
+import Recetas from "./pages/Recetas";
+import Ordenes from "./pages/Ordenes";
+import Ventas from "./pages/Ventas";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<Dashboard />} />
+
+        <Route
+          path="/pacientes"
+          element={<Pacientes />}
+        />
+
+        <Route
+          path="/agenda"
+          element={<Agenda />}
+        />
+
+        <Route
+          path="/historia-clinica"
+          element={<HistoriaClinica />}
+        />
+
+        <Route
+          path="/recetas"
+          element={<Recetas />}
+        />
+
+        <Route
+          path="/ordenes"
+          element={<Ordenes />}
+        />
+
+        <Route
+          path="/ventas"
+          element={<Ventas />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
