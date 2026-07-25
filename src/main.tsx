4d53ed12c982d5f0ec2efcde50a75fa1edb3b8cd
ahorrovision-ERP <ahorro.vision@gmail.com>
2026-07-25 13:38:29 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 import './styles/theme.css'
+import './styles/print.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
